0016afdbcc666725a4d96098bde65595a2941024
PascalEstime <nicintijep@gmail.com>
2024-05-27 11:00:30 +0300

    completes lab

--- lib/person.py
+++ lib/person.py
@@ -3,5 +3,14 @@
 class Person:
     # Class body goes here
 
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
     #Instance method definition
     pass
+
+jim = Person()
+jim.talk()
+jim.walk()
